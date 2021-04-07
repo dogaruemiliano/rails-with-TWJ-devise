154c53b22fb66e993cd16bf57babc000c319b929
dogaruemiliano <dogaru.emiliano@gmail.com>
2021-04-08 00:45:51 +0300

    Added Contact models validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Contact.destroy_all
+
+Contact.create(email: "dogy@gmail.com", first_name: "emilian", last_name: "dogy")
+Contact.create(email: "dog@gmail.com", first_name: "emi", last_name: "dog")
