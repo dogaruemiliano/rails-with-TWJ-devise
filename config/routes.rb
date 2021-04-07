f39d735b00b30a8cb222cbc8471356729c43a981
dogaruemiliano <dogaru.emiliano@gmail.com>
2021-04-08 00:10:22 +0300

    Fixed route bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources: :contacts, only: { :index, :create, :destroy }
+      resources :contacts, only: { :index, :create, :destroy }
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
