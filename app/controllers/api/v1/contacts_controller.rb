e8947364312c243a29d8fe9b741ff9b4e3958d10
dogaruemiliano <dogaru.emiliano@gmail.com>
2021-04-07 23:57:36 +0300

    Added Contact's model and controller

---
+++ app/controllers/api/v1/contacts_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+
+    render json: @contacts
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      render json: @contact, status: :ok
+    else
+      render json: { successfull: "ERROR: Not a valid model" }
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+
+    if @contact.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email)
+  end 
+end
